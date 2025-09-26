074edfad780157c618de5736798b68e7429dec0d
emiskas <emilispaulauskas279@gmail.com>
2025-09-26 12:28:54 +0300

    Make the automatic gunicorn restart script work for all files and refine instructions.

--- auto-gunicorn-restart/watcher.sh
+++ auto-gunicorn-restart/watcher.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-WATCH_DIR="/home/youruser/projects"
+
+# WATCH_DIR must target the directory where you want to watch for any changes
+WATCH_DIR="/home/youruser/"
+
+DEBOUNCE=3   # seconds to wait after a save before restarting
 
 inotifywait -m -r -e close_write --format '%w%f' "$WATCH_DIR" |
 while read -r file; do
-    if [[ "$file" == *.py ]]; then
-        echo "Restarting Gunicorn because $file changed"
-        sudo systemctl restart gunicorn
-    fi
+    logger "Watcher detected change in $file"
+    (
+        sleep $DEBOUNCE
+        systemctl restart gunicorn
+        logger "Gunicorn restarted by watcher"
+    ) &
 done
